b3c5c93eef1a2b0f01f8bd091a0acf91497b3aa7
didinirfandy <didinirfandy16@gmail.com>
2022-12-21 01:52:23 +0700

    revisi 1

--- application/views/request/request_form.php
+++ application/views/request/request_form.php
@@ -1,68 +1,80 @@
 <section class="content-header">
-      <h1>
-       Request
-       <small>Permintaan Barang</small>
-      </h1>
-      <ol class="breadcrumb">
+    <h1>
+        Request
+        <small>Permintaan Barang</small>
+    </h1>
+    <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
         <li class="active">Request</li>
-      </ol>
-    </section>
+    </ol>
+</section>
 
 
 <section class="content">
-<?php $this->view('messages')?>
+    <?php $this->view('messages') ?>
     <div class="box">
 
         <div class="box-header">
-            <h3 class="box-title"><?=ucfirst($page)?> Request</h3>
-                <div class="pull-right">
-                    <a href="<?=site_url('request')?>" class="btn btn-warning btn-flat">
-                       <i class="fa fa-user-undo"></i> Back
-                    </a>
-                </div>
+            <h3 class="box-title"><?= ucfirst($page) ?> Request</h3>
+            <div class="pull-right">
+                <a href="<?= site_url('request') ?>" class="btn btn-warning btn-flat">
+                    <i class="fa fa-user-undo"></i> Back
+                </a>
+            </div>
         </div>
-            <div class="box-body">
-                <div class="row">
-                    <div class="col-md-4 col-md-offset-4">
-                        <form action = "<?=site_url ('Request/process') ?>" method="post">
+        <div class="box-body">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <form action="<?= site_url('Request/process') ?>" method="post">
                         <div class="form-group">
-                                <label> Tanggal </label>
-                                <input type="date" name="date"  class="form-control" required>
+                            <label> Tanggal </label>
+                            <div class="input-group date">
+                                <div class="input-group-addon">
+                                    <i class="fa fa-calendar"></i>
+                                </div>
+                                <input type="text" class="form-control pull-right" id="datepicker">
                             </div>
+                            <!-- <input type="date" name="date" class="form-control" required> -->
+                        </div>
 
-                            <div class="form-group">
-                                <label> Jenis Kayu </label>
-                                <input type="hidden" name="id" value="<?=$row->request_id?>">
-                                <input type="text" name="jenis" value="<?=$row->jenis?>" class="form-control" required>
-                            </div>
-                        
-                            <div class="form-group">
-                                <label> Ukuran</label>
-                                <input type="text" name="ukuran" value="<?=$row->ukuran ?>" class="form-control">
-                            </div>
+                        <div class="form-group">
+                            <label> Jenis Kayu </label>
+                            <input type="hidden" name="id" value="<?= $row->request_id ?>">
+                            <input type="text" name="jenis" value="<?= $row->jenis ?>" class="form-control" required>
+                            <!-- <select class="form-control">
+                                <option>option 1</option>
+                                <option>option 2</option>
+                                <option>option 3</option>
+                                <option>option 4</option>
+                                <option>option 5</option>
+                            </select> -->
+                        </div>
 
-                            <div class="form-group">
-                                <label> Alasan </label>
-                                <input type="text" name="alasan" value="<?=$row->keterangan ?>" class="form-control">
-                            </div>
+                        <div class="form-group">
+                            <label> Ukuran</label>
+                            <input type="text" name="ukuran" value="<?= $row->ukuran ?>" class="form-control">
+                        </div>
 
-                            <div class="form-group">
-                                    <label>  Jumlah </label>
-                                    <input type="number" name="jumlah" class="form-control" required>
-                            </div>
-                        
+                        <div class="form-group">
+                            <label> Alasan </label>
+                            <input type="text" name="alasan" value="<?= $row->keterangan ?>" class="form-control">
+                        </div>
 
-                            <div class="form-group">
-                                <button type= "submit" name=<?=$page?> class="btn btn-success btn-flat"> <i class="fa fa-paper-plane"></i>Save</button>
-                                <button type= "reset"class="btn btn-warning btn-flat">Reset</button>
-                            </div>
-                        </form>
-                    </div>
+                        <div class="form-group">
+                            <label> Jumlah </label>
+                            <input type="number" name="jumlah" class="form-control" required>
+                        </div>
+
+                        <div class="form-group">
+                            <button type="submit" name=<?= $page ?> class="btn btn-success btn-flat"> <i class="fa fa-paper-plane"></i>Save</button>
+                            <button type="reset" class="btn btn-warning btn-flat">Reset</button>
+                        </div>
+                    </form>
                 </div>
-                    
             </div>
 
-          
+        </div>
+
+
     </div>
 </section>
